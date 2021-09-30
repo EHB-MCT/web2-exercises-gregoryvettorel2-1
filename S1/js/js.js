5f8ef081f8839d4f1b896d675f580f46498b503a
Gregory Vettorel <gregory.vettorel@student.ehb.be>
2021-09-30 12:58:39 +0200

    verbeterd

--- S1/js/js.js
+++ S1/js/js.js
@@ -1,8 +1,7 @@
 window.onload = () => {
     window.alert("page is loaded");
-}
 
-logSubmit = () => {
+document.getElementById('form').addEventListener('submit', event => {
     console.log("submited!");
     event.preventDefault();
     let order = {};
@@ -11,19 +10,19 @@ logSubmit = () => {
     let email = document.getElementById("email").value;
     let theOrder = document.getElementById("order").value;
 
-/** FROM EXCERCISE 3
-    const bigString = `<br><p>The order for the customer ${name}  is the following: ${order}. The customer may be notified by email: ${email}<p>`;
+    const bigString = `<br><p>The order for the customer ${name}  is the following: ${theOrder}. The customer may be notified by email: ${email}<p>`;
 
     document.getElementById("placeholder").innerHTML = bigString;
-*/
+
     order = {
         name: `${name}`,
         email: `${email}`,
         order: `${theOrder}`
     }
 
-}
+});
 
 printOrder = (orderDetails) => {
 
+    }
 }
